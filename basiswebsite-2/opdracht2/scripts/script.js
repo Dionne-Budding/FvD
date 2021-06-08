d16189924559dbe1a26ba37b114c3e512be7615c
Dionne-Budding <74144758+Dionne-Budding@users.noreply.github.com>
2021-06-08 17:46:43 +0200

    Add files via upload

---
+++ basiswebsite-2/opdracht2/scripts/script.js
@@ -0,0 +1,211 @@
+// https://codepen.io/shooft/pen/bGpLVev?editors=0010
+
+/* omdat er meer buttons geselecteerd moeten worden, wordt querySelectorAll gebruikt (en niet querySelector) */
+var hearts = document.querySelectorAll("button.favo");
+
+// pop up filteren
+var filteren = document.querySelector(".filteren");
+var openFilter = document.querySelector(".openFilter");
+
+// Voor het wisselen van view
+var view = document.querySelector("button[name='wissel-view']");
+
+/* alle button met class 'wish' in de HTML opzoeken */
+/* omdat er meer buttons geselecteerd moeten worden, wordt querySelectorAll gebruikt (en niet querySelector) */
+/* het gevonden lijstje met buttons (een array) in de variabele 'hearts' opslaan */
+var play = document.querySelectorAll("img.play");
+
+// opzoeken
+var options = {valueNames: [ 'name', 'artiest', 'genre' ]};
+var charactersList = new List('theList', options);
+
+
+/* 1. zoek de 4 radio buttons op en sla die op in een variabele */
+// document.querySelector
+var optionAll = document.querySelector("#filter-all");
+var optionPop = document.querySelector("#filter-pop");
+var optionRnb = document.querySelector("#filter-rnb");
+var optionHiphop = document.querySelector("#filter-hiphop");
+var optionDance = document.querySelector("#filter-dance");
+
+/* de opties om het sorteren te initialiseren */
+var sort = {
+  // de classes van de h2's en p's (daarin gaat de lijst gesorteerd worden)
+  valueNames: [ 'name' ]
+};
+
+/* de opties om het draggen-en-droppen te initialiseren */
+var options = {
+  // de tijd van swappen in ms
+  animation: 1000
+}
+
+/* het daadwerkelijk initialiseren van het draggen-en-droppen */
+// list - de class van de ul
+// options - hierboven gedefinieerd
+var deLijst = document.querySelector(".list");
+var sortable = Sortable.create(deLijst, options);
+
+// Muziek
+var button = document.querySelector(".pause");
+var audio = document.querySelector(".audio");
+
+// Functie hartje laten vullen
+function addToFavorites(event) {
+  /* het hartje waarop geklikt is in de variabele 'clickedHeart' opslaan */
+  var clickedHeart = event.target;
+
+  /* als het hartje niet gevuld is */
+  if (clickedHeart.innerHTML == "\u2661") {
+    /* het niet gevulde hartje vervangen door gevuld */
+    clickedHeart.innerHTML = "\u2665";
+    /* en dan de functie aanroepen om de wishlist aan te passen */
+    updatefavoriet("plus");
+  }
+  /* Als het hartje gevuld is */
+  else {
+    /* Het gevulde hartje vervangen door niet gevuld */
+    clickedHeart.innerHTML = "\u2661";
+    /* en dan de functie aanroepen om de love-list aan te passen */
+    updatefavoriet("min");
+  }
+}
+
+/* de favorieten updaten */
+/* de action die meegegeven wordt, is "plus" of "min" */
+function updatefavoriet(action) {
+  /* het getal (de span) in de Wishlist-link in de nav opzoeken */
+  /* die span in de variabele 'loveListAmount' opslaan */
+  let wishlistAmount = document.querySelector(".favoriet span");
+
+  /* het huidige aantal gele hartjes bepalen */
+  let currentAmount = wishlistAmount.innerHTML;
+  /* dat is een string - dus even omzetten naar een getal */
+  currentAmount = parseInt(currentAmount);
+
+  /* het nieuwe aantal gele hartjes bepalen */
+  /* te beginnen met een variabele om het nieuwe aantal in op te slaan */
+  let newAmount;
+
+  /* als er liefde bij moet */
+  if (action == "plus") {
+    /* het nieuwe aantal gele hartjes berekenen */
+    newAmount = currentAmount + 1;
+  }
+
+  /* als er liefde af moet */
+  else {
+    /* het nieuwe aantal gele hartjes berekenen */
+    newAmount = currentAmount - 1;
+  }
+
+  /* tenslotte het nieuwe aantal in de HTML zetten */
+  wishlistAmount.innerHTML = newAmount;
+}
+
+// Functie voor filteren button
+function filterenOp(){
+  openFilter.classList.toggle("open");
+  filteren.classList.toggle("dicht");
+}
+
+// functie wisselview
+function wisselview() {
+  document.body.classList.toggle("grid-view");
+}
+
+// Credits naar Sam!!!!!
+// Wissel van play naar pause en andersom
+function wissel(event) {
+  var clickedPlay = event.target;
+  if (!clickedPlay.classList.contains('pausedButton')) {
+     clickedPlay.src = "images/pause.svg";
+     clickedPlay.classList.add('pausedButton')
+  }
+  else {
+    clickedPlay.src = "images/play.svg";
+    clickedPlay.classList.remove('pausedButton')
+  }
+}
+
+/* 3. maak die functie aan */
+// zoek de ul op en stop die in een variabele
+// sla de value van de gekozen radio button op in een variabele --> event.target.value
+// verwijder de huidige class van de ul --> deLijst.className = "";
+// voeg de de nieuwe value als class toe aan de ul --> gebruik daarvoor de variabele van 2 regels omhoog
+function filterList() {
+  var deLijst = document.querySelector("ul");
+  var nieuweFilter = event.target.value;
+
+  deLijst.className = "";
+  deLijst.classList.add(nieuweFilter);
+}
+
+function openFilterMenu(event) {
+  /* de aside opzoeken met document.querySelector */
+  /* en die in een variabele stoppen */
+  let deAside = document.querySelector("aside.openFilter");
+  /* bepalen welke toets is ingedrukt */
+  switch(event.key) {
+    /* als dat de ArrowRight is dan dit doen */
+    case "ArrowRight":
+      deAside.classList.add("toonMenu");
+      /* break - zodat de andere toetsen niet onnodig gecheckt worden */
+      break;
+    /* als dat de ArrowLeft is dan dit doen */
+    case "ArrowLeft":
+      deAside.classList.remove("toonMenu");
+      break;
+  }
+}
+
+// Muziek https://jsfiddle.net/hibbard_eu/enoqwg5b/
+button.addEventListener("click", function(){
+  if(audio.paused){
+    audio.play();
+
+  } else {
+    audio.pause();
+
+  }
+});
+
+
+/* elke button laten luisteren naar een klik in een array */
+hearts[0].addEventListener("click", addToFavorites);
+hearts[1].addEventListener("click", addToFavorites);
+hearts[2].addEventListener("click", addToFavorites);
+hearts[3].addEventListener("click", addToFavorites);
+hearts[4].addEventListener("click", addToFavorites);
+hearts[5].addEventListener("click", addToFavorites);
+
+
+// filteren
+filteren.addEventListener("click", filterenOp);
+
+// wissel van view
+view.addEventListener("click", wisselview);
+
+
+/* 2. laat de radio buttons luisteren naar wijzigingen */
+// addEventListener
+// roep dan steeds dezelfde functie aan
+optionAll.addEventListener("change", filterList);
+optionPop.addEventListener("change", filterList);
+optionRnb.addEventListener("change", filterList);
+optionHiphop.addEventListener("change", filterList);
+optionDance.addEventListener("change", filterList);
+
+
+/* elke button laten luisteren naar een klik */
+/* na een klik de functie 'toggleLove' uitvoeren */
+play[0].addEventListener("click", wissel);
+play[1].addEventListener("click", wissel);
+play[2].addEventListener("click", wissel);
+play[3].addEventListener("click", wissel);
+play[4].addEventListener("click", wissel);
+play[5].addEventListener("click", wissel);
+
+/* het document luistert naar toetsaanslagen */
+/* bij een toetsaanslag wordt de functie openMenuMetToetsen uitgevoerd */
+document.addEventListener('keydown', openFilterMenu);
